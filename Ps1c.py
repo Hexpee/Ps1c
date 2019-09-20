146202dba0f1b9e248458d2c5282e5e572ea8772
Hexpee <52126266+Hexpee@users.noreply.github.com>
2019-09-20 15:21:47 +0100

    Update Ps1c.py

--- Ps1c.py
+++ Ps1c.py
@@ -1,33 +1,33 @@
 #program that calculate a approriate time ot take to save upto 1m in 36 month using Bisection and guess
-annualSalary = float(input("Enter your annual salary: "))
+annual_salary = float(input("Enter your annual salary: "))
 #portion saved
-portionSaved = float(input("Enter the percent of your salary to save, in decimal: "))
-totalCost = float(1000000)
+portion_saved = float(input("Enter the percent of your salary to save, in decimal: "))
+total_cost = float(1000000)
 # semi annual raise incrsement....
-semiAnnualRaise = float(0.7)
+semi_annual_raise = float(0.7)
 #to get the portion down payment 
-portionDownPayment = totalCost * 0.25
+portion_down_payment = total_cost * 0.25
 #initializing the current saving getting a return value from monthly salary X by portion saved plus value of current+
-currentSavings = 0
+current_savings = 0
 r = 0.04
 months = 36
-while currentSavings < portionDownPayment:
+while current_savings < portion_down_payment:
     months += 1
     if months%6 == 0:
-        annualSalary += annualSalary * semiAnnualRaise
+        annual_salary += annual_salary * semi_annual_raise
 #apropriate time variable
-pTime=totalCost
-semiAnnualRaise=0.7
+p_time=total_cost
+semi_annual_raise=0.7
 r=0
-portionDownPayment=0
-guess=(pTime+r) /2.0
-while abs (guess**3-pTimd) >=semiAnnualRaise:
-	if guess**3 < pTime:
-		r=pTime
+portion_down_payment=0
+guess=(p_time+r) /2.0
+while abs (guess**3-p_time) >=semi_annual_raise:
+	if guess**3 < p_time:
+		r=p_time
 else:
-	portionDownPayment=pTime
-portionDownPayment=(guess-pTime)/2.0
-guess (r + portionDownPayment) /2.0
-currentSavings += (annualSalary/12)*portionSaved + currentSavings*(r/12)
-print (currentSaving,"best saving rate:",)
+	portion_down_payment=p_time
+portion_down_payment=(guess-p_time)/2.0
+guess (r + portion_down_payment) /2.0
+current_savings += (annual_salary/12)*portion_saved + current_savings*(r/12)
+print (current_saving,"best saving rate:",)
 print (guess,"Steps in Bisection search is :",)
